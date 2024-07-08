dc5094507afc839b1ab96576e6fb1b20fb2a3e94
Yousef-Binnob <yusufbinnob7111@gmail.com>
2024-07-08 13:36:26 +0300

    - added refrence to business layer - designed main form with listing contact only

--- WinFormContacts/Form1.Designer.cs
+++ WinFormContacts/Form1.Designer.cs
@@ -29,12 +29,83 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dgvContacts = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.updaeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvContacts
+            // 
+            this.dgvContacts.AllowUserToAddRows = false;
+            this.dgvContacts.AllowUserToDeleteRows = false;
+            this.dgvContacts.AllowUserToOrderColumns = true;
+            this.dgvContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContacts.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvContacts.Location = new System.Drawing.Point(0, 96);
+            this.dgvContacts.Name = "dgvContacts";
+            this.dgvContacts.ReadOnly = true;
+            this.dgvContacts.RowHeadersWidth = 62;
+            this.dgvContacts.RowTemplate.Height = 29;
+            this.dgvContacts.Size = new System.Drawing.Size(1351, 491);
+            this.dgvContacts.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1150, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add New Contact";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updaeToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 68);
+            // 
+            // updaeToolStripMenuItem
+            // 
+            this.updaeToolStripMenuItem.Name = "updaeToolStripMenuItem";
+            this.updaeToolStripMenuItem.Size = new System.Drawing.Size(136, 32);
+            this.updaeToolStripMenuItem.Text = "Updae";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(136, 32);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1351, 587);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvContacts);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvContacts;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem updaeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
     }
 }
 
--- WinFormContacts/Form1.cs
+++ WinFormContacts/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bussiness_Layer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,16 @@ namespace WinFormContacts
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _Refresh();
+        }
+
+        private void _Refresh()
+        {
+            dgvContacts.DataSource = clsContact.ListContacts();
         }
     }
 }
